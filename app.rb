1eec4c7dc35478f88fc056f5672b20827e5d0a77
meredith winchell <meredithwinchell@gmail.com>
2018-06-12 15:26:40 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    "Welcome to your app!!!! HEY BOYSS!! "
+    "Welcome to your app!!!! HI I CAN CODE!! "
   end
 
 end
